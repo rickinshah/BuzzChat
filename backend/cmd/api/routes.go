0c4910e9b82d676660af1a719ea6d3a624ef4404
Rickin Shah <rickin.shah17403@gmail.com>
2025-05-05 18:14:18 +0530

    change in backend api path

--- backend/cmd/api/routes.go
+++ backend/cmd/api/routes.go
@@ -6,9 +6,9 @@ func (app *application) routes() http.Handler {
 	router := http.NewServeMux()
 
 	router.HandleFunc("GET /v1/healthcheck", app.healthcheckHandler)
-	router.HandleFunc("GET /v1/user/{username}", app.requireAuthenticatedUser(app.getUserHandler))
-	router.HandleFunc("POST /v1/user", app.registerUserHandler)
-	router.HandleFunc("GET /v1/user/profile", app.requireAuthenticatedUser(app.getProfileHandler))
-	router.HandleFunc("POST /v1/user/authentication", app.authenticationHandler)
+	router.HandleFunc("GET /v1/users/{username}", app.requireAuthenticatedUser(app.getUserHandler))
+	router.HandleFunc("POST /v1/auth/register", app.registerUserHandler)
+	router.HandleFunc("GET /v1/users/me", app.requireAuthenticatedUser(app.getProfileHandler))
+	router.HandleFunc("POST /v1/auth/login", app.authenticationHandler)
 	return app.authenticate(router)
 }
